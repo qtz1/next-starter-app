d9d8787d16469de335e446e1e1e904a09b847916
Brian Li <blidd@google.com>
2024-12-11 11:09:26 -0500

    missing lockfile

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    asdf
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
